adb6b2c20b336ca257f96f366e154afc851cb9b4
Davi Chalita <chalitameneguelli14@gmail.com>
2024-04-17 11:42:43 -0300

    att host 2

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ if __name__ == '__main__':
     scheduler.init_app(app)
     scheduler.start()
     app.logger.setLevel(logging.INFO)
-    app.run(host="44.226.145.213")
+    app.run(host="0.0.0.0")
